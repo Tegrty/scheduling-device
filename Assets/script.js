2930a922b28fce4c1800f9a33f4d208bc6e04cf2
Mario Repas <mario.repas63@gmail.com>
2023-01-26 21:09:39 -0500

    final commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   const hourNine = document.getElementById('hour-9');
   const hourTen = document.getElementById('hour-10');
@@ -13,6 +10,7 @@ $(function () {
   const hourSeventeen = document.getElementById('hour-17');
   const saveBtn = document.querySelectorAll('.btn');
 
+ // Array to compare with Dayjs.hour
   let hourList = [
     "",
     "",
@@ -47,14 +45,39 @@ $(function () {
 // function to save content to local storage
   $( saveBtn ).click(function() {
         // Get nearby values of the description in JQuery
-        let text = $(this).siblings(".description").val();
+        let text = $(this).siblings("textarea").val();
         let time = $(this).parent().attr("id");
         // Save text in local storage
         localStorage.setItem(time, text);
   });
 
+ // Values to retrieve data from local storage
+  let hourNineValue= localStorage.getItem('hour-9')
+  $('#hour-9 textarea').val(hourNineValue)
 
+  let hourTenValue= localStorage.getItem('hour-10')
+  $('#hour-10 textarea').val(hourTenValue)
 
+  let hourElevenValue= localStorage.getItem('hour-11')
+  $('#hour-11 textarea').val(hourElevenValue)
+
+  let hourTwelveValue= localStorage.getItem('hour-12')
+  $('#hour-12 textarea').val(hourTwelveValue)
+
+  let hourThirteenValue= localStorage.getItem('hour-13')
+  $('#hour-13 textarea').val(hourThirteenValue)
+
+  let hourFourteenValue= localStorage.getItem('hour-14')
+  $('#hour-14 textarea').val(hourFourteenValue)
+
+  let hourFifteenValue= localStorage.getItem('hour-15')
+  $('#hour-15 textarea').val(hourFifteenValue)
+
+  let hourSixteenValue= localStorage.getItem('hour-16')
+  $('#hour-16 textarea').val(hourSixteenValue)
+
+  let hourSeventeenValue= localStorage.getItem('hour-17')
+  $('#hour-17 textarea').val(hourSeventeenValue)
 
   setInterval(() =>
 {
@@ -63,24 +86,7 @@ $(function () {
 },1000);
 
   console.log(dayjs().hour());
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
 
 
